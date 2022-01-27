1ea52025fd33e27b0317ec2150146213ab3e7781
LukeRouleau <lucasluke2000@gmail.com>
2022-01-26 21:18:48 -0500

    Added rviz_demo_ws
    Remote Development with Rviz is setup

---
+++ ROS_Melodic_Implementation/rviz_demo_ws/build/using_markers/CMakeFiles/basic_shapes.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "/home/luke/workspace/rviz_test_ws/devel/lib/using_markers/basic_shapes"
+  "/home/luke/workspace/rviz_test_ws/devel/lib/using_markers/basic_shapes.pdb"
+  "CMakeFiles/basic_shapes.dir/src/basic_shapes.cpp.o"
+  "CMakeFiles/basic_shapes.dir/src/basic_shapes.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/basic_shapes.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
